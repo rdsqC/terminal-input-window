e0a17e89aa269f78d22925d510151a75e4b89478
rdsqC <pdiamondq49@gmail.com>
2024-11-12 13:20:29 +0900

    入力一列を表すOrderコンポーネントの定義
    先ほど定義したInputBoxを作成して並べる。
    InpuBoxにつける属性は配列として inputs という引数がうけとる。inputsの各要素は title, type, backtext, inputpropsの4つのkeyを持ったオブジェクトであり、これはInputBoxの引数に一対一で対応する。ちなみにforをAarryの_proto_の中にあるmap関数に今すぐにでも書き換えたくてしょうがないが、PCのスペックがなさ過ぎて S-tab (今書いているPC) だとマウスが過去の位置に勝手に移動したりして話にならないので今すぐにはやめておく。

--- seatmaster/src/App.js
+++ seatmaster/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function ViewPort(){
   return(
     <div className='viewPort'>
@@ -8,12 +9,24 @@ function ViewPort(){
     </div>
   )
 }
+
 function InputBox({ title, type, backtext, inputprops}){
   return(
     <label htmlFor={title} className="inputBox">
         <span>{title}</span>
         <input id={title} type={type} placeholder={backtext} {...inputprops}/>
     </label>
+  )
+}
+function Order({ inputs }){
+  let inputElements = [];
+  for(let input of inputs){
+    inputElements.push(
+      <InputBox key={input.title} title={input.title} type={input.type} backtext={input.backtext} inputprops={input.inputprops}/>
+    );
+  }
+  return(
+    <div className='order'>{inputElements}</div>
   )
 }
 function App() {
